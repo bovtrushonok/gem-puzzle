b6c903a0fac370171193271356bdc720218699b5
Marie Bovtramovich <m.abovt@mail.ru>
2020-11-12 23:56:04 +0300

    add: numbers gem-puzzle work

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const GemPuzzle = {
         main: null, //board
         menu: null,
         puzzlesContainer: null, //puzzles area
-        puzzles: [], // puzzles
-    },
+        puzzles: [], // HTML elements puzzles
+     },
 
     properties: {
-        tilesSequence: [],
-        puzzlesArray: null,
+        size: null,
+        emptyTile: null,
+        emptyTileRow: null,
+        emptyTileColumn: null,
+        tilesSequence: [], // initial puzzles content sequence
+        puzzlesArray: null, //current puzzles sequence
     },
 
     init() {
@@ -24,7 +28,6 @@ const GemPuzzle = {
         
         this.createTilesSequence(9);//invoke on button to chooze size of field
         this.createPuzzle(9); //invoke on button to chooze size of field 
-
         this.elements.puzzles = Array.from(this.elements.puzzlesContainer.querySelectorAll('.gameboard__puzzle__tile'));
         this.createPuzzlesArray(9); //invoke on button to choose size of field
     },
@@ -34,7 +37,8 @@ const GemPuzzle = {
             let randomNumber = this.randomInteger(1, 9);  
             if (this.properties.tilesSequence.includes(randomNumber)) --i;
             else this.properties.tilesSequence.push(randomNumber);   
-        };           
+        };        
+        console.log(this.properties.tilesSequence);   
     },
 
     randomInteger(min, max) { // случайное число от min до (max+1)
@@ -43,40 +47,68 @@ const GemPuzzle = {
     },
 
     createPuzzlesArray(puzzleSize) { //form 2d array of current field situation
-        this.properties.puzzlesArray = new Array(Math.sqrt(puzzleSize));
+        this.properties.puzzlesArray = new Array(this.properties.size);
         for (let j = 0; j < this.properties.puzzlesArray.length; j++) {
-            this.properties.puzzlesArray[j] = new Array(Math.sqrt(puzzleSize));
+            this.properties.puzzlesArray[j] = new Array(this.properties.size);
         };
-        console.log(this.properties.puzzlesArray);
         let i = 0;
-        for (let j = 0; j < Math.sqrt(puzzleSize); j++) {
-            for (let k = 0; k < Math.sqrt(puzzleSize); k++) {
+        for (let j = 0; j < this.properties.size; j++) {
+            for (let k = 0; k < this.properties.size; k++) {
                this.properties.puzzlesArray[j][k] = this.elements.puzzles[i];
                i++;
-               console.log(this.properties.puzzlesArray);
-               this.properties.puzzlesArray[j][k].addEventListener('click', () => {
-                    if (this.properties.puzzlesArray[j][k+1].classList.contains('empty')) {       
-                        this.properties.puzzlesArray[j][k+1].style.order = this.properties.puzzlesArray[j][k+1].style.order * 1 - 1;  
-                        this.properties.puzzlesArray[j][k].style.order = this.properties.puzzlesArray[j][k].style.order * 1 + 1;
-           
-                        //[this.elements.puzzles[j][k], this.elements.puzzles[j][k+1]] = [this.elements.puzzles[j][k+1], this.elements.puzzles[j][k]];
-                        console.log(this.properties.puzzlesArray);
-                    }       
-                });
             };
         };
-   
-       /* for(let i = 0; i < puzzleSize; i++) {
-            this.elements.puzzles[i].addEventListener('click', () => {
-               
-                if (this.elements.puzzles[i+1].classList.contains('empty')) {      
-
-                    this.elements.puzzles[i+1].style.order = this.elements.puzzles[i+1].style.order * 1 - 1;
-                    this.elements.puzzles[i].style.order = this.elements.puzzles[i].style.order * 1 + 1;         
-                    [this.elements.puzzles[i], this.elements.puzzles[i+1]] = [this.elements.puzzles[i+1], this.elements.puzzles[i]]; 
+        this.defineClickableTiles();
+    },
+    
+    changeSequence(e) { //to change orders of tiles 
+        let currentTileColumn;
+        let currentTileRow;
+        if (e.target.classList.contains('empty')) return; 
+        if (e.target.classList.contains('clickable')) {
+            let currentOrder =  e.target.style.order;
+            let emptyOrder = this.properties.emptyTile.style.order
+            e.target.style.order = emptyOrder;
+            this.properties.emptyTile.style.order = currentOrder;
+            for (let j = 0; j < this.properties.size; j++) {
+                if (this.properties.puzzlesArray[j].indexOf(e.target) !== -1) {
+                    currentTileRow = j;
+                    currentTileColumn = this.properties.puzzlesArray[j].indexOf(e.target);
+                    
                 }
-            });
-        };*/
+                for (let k = 0; k < this.properties.size; k++) {
+                    this.properties.puzzlesArray[j][k].classList.remove("clickable");
+                };
+            };
+            //   console.log(currentTileRow, currentTileColumn, this.properties.emptyTileRow, this.properties.emptyTileColumn);
+         [this.properties.puzzlesArray[currentTileRow][currentTileColumn], this.properties.puzzlesArray[this.properties.emptyTileRow][this.properties.emptyTileColumn]] = [this.properties.puzzlesArray[this.properties.emptyTileRow][this.properties.emptyTileColumn], this.properties.puzzlesArray[currentTileRow][currentTileColumn]];
+           // console.log(this.properties.puzzlesArray);
+        }
+
+        this.defineClickableTiles()
+    },      
+
+    defineClickableTiles() {
+        let emptyTile;
+        let row;
+        let column;
+        for (let j = 0; j < this.properties.size; j++) {
+            for (let k = 0; k < this.properties.size; k++) {
+               if (this.properties.puzzlesArray[j][k].classList.contains('empty')) {
+                emptyTile = this.properties.puzzlesArray[j][k];
+                 row = j;
+                 column = k;
+                if (k < this.properties.size - 1) this.properties.puzzlesArray[j][k + 1].classList.add('clickable');
+                if (k > 0) this.properties.puzzlesArray[j][k - 1].classList.add('clickable');
+                if (j > 0) this.properties.puzzlesArray[j - 1][k].classList.add('clickable');
+                if (j < this.properties.size - 1) this.properties.puzzlesArray[j + 1][k].classList.add('clickable');
+               };
+            };
+        };
+      this.properties.emptyTile = emptyTile;
+      this.properties.emptyTileRow = row;
+      this.properties.emptyTileColumn = column;
+   
     },
 
     createPuzzle(puzzleSize) {
@@ -86,11 +118,14 @@ const GemPuzzle = {
             puzzleTile.textContent = this.properties.tilesSequence[i];
             puzzleTile.style.order = i;
     
-            if (i == puzzleSize - 1) {
-                puzzleTile.classList.add('empty');
-            };
+            if (puzzleTile.textContent == 9) puzzleTile.classList.add('empty');
+            puzzleTile.addEventListener('click', (e) => {
+                this.changeSequence(e);
+            });
             this.elements.puzzlesContainer.append(puzzleTile);
         };
+        this.properties.size = Math.sqrt(puzzleSize);
+ 
     },
 }
 
